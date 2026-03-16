a6f03c0c6bb3488a9d91848ad1ea25ef76d0a5ee
Noam Cohen <117226467+Noam173@users.noreply.github.com>
2026-03-16 17:10:21 +0200

    Refactor grep to process and support file input
    Refactor grep function into process function and add file reading support.

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,70 @@
 use clap::Parser;
-use std::io::{Result, stdin};
+use std::fs::File;
+use std::io::{BufRead, BufReader, Result, stdin};
+
 #[derive(Parser)]
 struct Args {
     #[arg(short, long)]
     insensitive: bool,
+
     #[arg(short = 'v', long)]
     exclude: bool,
+
     #[arg(short, long)]
     count: bool,
+
     query: String,
+    files: Vec<String>,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
-    grep(args)
-}
-fn grep(args: Args) -> Result<()> {
-    let lines = stdin().lines();
-    let mut matched: Vec<String> = Vec::new();
-    let l_query = args.query.to_lowercase();
 
-    for line in lines {
-        let line = line?;
-        let mut i: bool = if args.insensitive {
-            line.to_lowercase().contains(&l_query)
-        } else {
-            line.contains(&args.query)
-        };
-        if args.exclude {
-            i = !i;
+    if args.files.is_empty() {
+        let reader = stdin();
+        process(reader.lock(), &args)?;
+    } else {
+        for file in &args.files {
+            let reader = BufReader::new(File::open(file)?);
+            process(reader, &args)?;
         }
-        if i {
-            matched.push(line);
+    }
+
+    Ok(())
+}
+
+fn process<R: BufRead>(reader: R, args: &Args) -> Result<()> {
+    let query = if args.insensitive {
+        args.query.to_lowercase()
+    } else {
+        args.query.clone()
+    };
+
+    let mut count: u16 = 0;
+
+    for line in reader.lines() {
+        let line = line?;
+
+        let matched = if args.insensitive {
+            line.to_lowercase().contains(&query)
+        } else {
+            line.contains(&query)
+        };
+
+        let matched = if args.exclude { !matched } else { matched };
+
+        if matched {
+            if !args.count {
+                println!("{}", line);
+            } else {
+                count += 1;
+            }
         }
     }
 
     if args.count {
-        println!("{}", matched.len());
-    } else {
-        for i in matched {
-            println!("{}", i);
-        }
+        println!("{}", count);
     }
+
     Ok(())
 }
